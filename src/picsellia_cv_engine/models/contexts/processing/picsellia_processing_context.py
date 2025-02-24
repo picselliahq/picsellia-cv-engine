68de9b548932c43b94d66c2d6cf56230c8812d1e
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-02-24 17:53:05 +0100

    fix: imports

--- src/picsellia_cv_engine/models/contexts/processing/picsellia_processing_context.py
+++ src/picsellia_cv_engine/models/contexts/processing/picsellia_processing_context.py
@@ -5,10 +5,10 @@ import picsellia  # type: ignore
 from picsellia import DatasetVersion, ModelVersion
 from picsellia.types.enums import ProcessingType
 
-from examples.processing.augmentation.main_augmentation import Parameters
 from picsellia_cv_engine.models.contexts.common.picsellia_context import (
     PicselliaContext,
 )
+from picsellia_cv_engine.models.parameters.base_parameters import Parameters
 
 TParameters = TypeVar("TParameters", bound=Parameters)
 
