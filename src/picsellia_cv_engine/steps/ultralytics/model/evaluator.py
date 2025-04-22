7418d2e638841788a2b60b0bc851996ee9968125
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-04-22 14:52:11 +0200

    feat: add ultralytics utils

---
+++ src/picsellia_cv_engine/steps/ultralytics/model/evaluator.py
@@ -0,0 +1,79 @@
+import os
+
+from picsellia_cv_engine import Pipeline, step
+from picsellia_cv_engine.core.contexts import PicselliaTrainingContext
+from picsellia_cv_engine.core.data import TBaseDataset
+from picsellia_cv_engine.core.models.ultralytics.model import UltralyticsModel
+from picsellia_cv_engine.core.parameters import ExportParameters
+from picsellia_cv_engine.core.parameters.ultralytics.augmentation_parameters import (
+    UltralyticsAugmentationParameters,
+)
+from picsellia_cv_engine.core.parameters.ultralytics.hyper_parameters import (
+    UltralyticsHyperParameters,
+)
+from picsellia_cv_engine.services.ultralytics.model.predictor.classification import (
+    UltralyticsClassificationModelPredictor,
+)
+from picsellia_cv_engine.services.ultralytics.model.predictor.object_detection import (
+    UltralyticsDetectionModelPredictor,
+)
+from picsellia_cv_engine.services.ultralytics.model.predictor.segmentation import (
+    UltralyticsSegmentationModelPredictor,
+)
+from picsellia_cv_engine.steps.base.model.evaluator import (
+    evaluate_model_impl,
+)
+
+
+@step
+def evaluate_ultralytics_model(
+    model: UltralyticsModel,
+    dataset: TBaseDataset,
+) -> None:
+    """
+    Evaluates an Ultralytics classification model on a given dataset.
+
+    This function retrieves the active training context from the pipeline, performs inference using
+    the provided Ultralytics classification model on the dataset, and evaluates the predictions. It processes
+    the dataset in batches, runs inference, and then logs the evaluation results to the experiment.
+
+    Args:
+        model (Model): The Ultralytics model to be evaluated.
+        dataset (TDataset): The dataset containing the data for evaluation.
+
+    Returns:
+        None: The function performs evaluation and logs the results to the experiment but does not return any value.
+    """
+    context: PicselliaTrainingContext[
+        UltralyticsHyperParameters, UltralyticsAugmentationParameters, ExportParameters
+    ] = Pipeline.get_active_context()
+
+    if model.loaded_model.task == "classify":
+        model_predictor = UltralyticsClassificationModelPredictor(model=model)
+    elif model.loaded_model.task == "detect":
+        model_predictor = UltralyticsDetectionModelPredictor(model=model)
+    elif model.loaded_model.task == "segment":
+        model_predictor = UltralyticsSegmentationModelPredictor(model=model)
+    else:
+        raise ValueError(f"Model task {model.loaded_model.task} not supported")
+
+    image_paths = model_predictor.pre_process_dataset(dataset=dataset)
+    image_batches = model_predictor.prepare_batches(
+        image_paths=image_paths, batch_size=context.hyperparameters.batch_size
+    )
+    batch_results = model_predictor.run_inference_on_batches(
+        image_batches=image_batches
+    )
+    picsellia_predictions = model_predictor.post_process_batches(
+        image_batches=image_batches,
+        batch_results=batch_results,
+        dataset=dataset,
+    )
+
+    evaluate_model_impl(
+        context=context,
+        picsellia_predictions=picsellia_predictions,
+        inference_type=model.model_version.type,
+        assets=dataset.assets,
+        output_dir=os.path.join(context.experiment.name, "evaluation"),
+    )
---
+++ src/picsellia_cv_engine/steps/ultralytics/model/exporter.py
@@ -0,0 +1,54 @@
+import logging
+
+from picsellia_cv_engine import Pipeline, step
+from picsellia_cv_engine.core.contexts import PicselliaTrainingContext
+from picsellia_cv_engine.core.models.ultralytics.model import UltralyticsModel
+from picsellia_cv_engine.core.parameters import ExportParameters
+from picsellia_cv_engine.core.parameters.ultralytics.augmentation_parameters import (
+    UltralyticsAugmentationParameters,
+)
+from picsellia_cv_engine.core.parameters.ultralytics.hyper_parameters import (
+    UltralyticsHyperParameters,
+)
+from picsellia_cv_engine.services.ultralytics.model.exporter import (
+    UltralyticsModelExporter,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@step
+def export_ultralytics_model(model: UltralyticsModel):
+    """
+    Exports and saves the Ultralytics model to the experiment.
+
+    This function retrieves the active training context from the pipeline, exports the Ultralytics model
+    in the specified format, and saves the exported model weights to the experiment. If the `exported_weights_dir`
+    is not found in the model, the export process is skipped, and a log message is generated.
+
+    Args:
+        model (Model): The model for the Ultralytics model to be exported and saved.
+
+    Raises:
+        If no `exported_weights_dir` is found in the model, the export process is skipped, and
+        a log message is generated.
+    """
+    context: PicselliaTrainingContext[
+        UltralyticsHyperParameters, UltralyticsAugmentationParameters, ExportParameters
+    ] = Pipeline.get_active_context()
+
+    model_exporter = UltralyticsModelExporter(model=model)
+
+    if model.exported_weights_dir:
+        model_exporter.export_model(
+            exported_model_destination_path=model.exported_weights_dir,
+            export_format=context.export_parameters.export_format,
+            hyperparameters=context.hyperparameters,
+        )
+        model_exporter.save_model_to_experiment(
+            experiment=context.experiment,
+            exported_weights_path=model.exported_weights_dir,
+            exported_weights_name="model-latest",
+        )
+    else:
+        logger.info("No exported weights directory found in model. Skipping export.")
