176d05e3eda45a8c1b20ace08bde3a9a3773f5f9
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-07-22 16:18:40 +0200

    fix: add missing params

--- src/picsellia_cv_engine/core/contexts/processing/datalake/__init__.py
+++ src/picsellia_cv_engine/core/contexts/processing/datalake/__init__.py
@@ -1,4 +1,4 @@
-from .local_datalake_processing_context import LocalDatalakeProcessingContext
-from .picsellia_datalake_processing_context import PicselliaDatalakeProcessingContext
+from .local_context import LocalDatalakeProcessingContext
+from .picsellia_context import PicselliaDatalakeProcessingContext
 
 __all__ = ["LocalDatalakeProcessingContext", "PicselliaDatalakeProcessingContext"]
