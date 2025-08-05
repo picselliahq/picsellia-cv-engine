62cc44d8592362e1af21d4be59dabd4e21284525
SoniaGrh <sonia.garrouch@hotmail.fr>
2025-08-05 14:28:34 +0200

    fix: load model

--- src/picsellia_cv_engine/steps/sam2/model/evaluator.py
+++ src/picsellia_cv_engine/steps/sam2/model/evaluator.py
@@ -3,38 +3,55 @@ import os
 import torch
 
 from picsellia_cv_engine import Pipeline, step
-from picsellia_cv_engine.core import CocoDataset, Model
+from picsellia_cv_engine.core import CocoDataset
 from picsellia_cv_engine.core.contexts import (
     LocalTrainingContext,
     PicselliaTrainingContext,
 )
 from picsellia_cv_engine.core.services.model.utils import evaluate_model_impl
+from picsellia_cv_engine.frameworks.sam2.model.model import SAM2Model
 from picsellia_cv_engine.frameworks.sam2.services.predictor import SAM2ModelPredictor
 
 
 @step
-def evaluate(model: Model, dataset: CocoDataset):
+def evaluate(model: SAM2Model, dataset: CocoDataset):
     """
-    Étape d'évaluation pour SAM2. Effectue une inférence complète sur le dataset
-    et loggue les résultats dans l'expérience Picsellia.
+    Evaluation step for the SAM2 model using a given COCO dataset.
+
+    This step performs full inference on all dataset images using the trained SAM2 model,
+    then post-processes the output into Picsellia-compatible predictions and evaluates them.
+
+    Args:
+        model (Model): The trained Picsellia SAM2 model to be evaluated.
+        dataset (CocoDataset): The COCO-format dataset used for evaluation.
+
+    Returns:
+        None
     """
     context: PicselliaTrainingContext | LocalTrainingContext = (
         Pipeline.get_active_context()
     )
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    predictor = SAM2ModelPredictor(model=model, device=device)
+    if not model.trained_weights_path:
+        raise FileNotFoundError("No trained weights path found in model.")
 
-    # 📂 Récupérer les chemins d’image
+    if not model.config_path:
+        raise FileNotFoundError("No configuration path found in model.")
+
+    loaded_model, loaded_generator = model.load_weights(
+        weights_path=model.trained_weights_path,
+        config_path=model.config_path,
+        device=device,
+    )
+    model.set_loaded_model(loaded_model)
+    model.set_loaded_generator(loaded_generator)
+
+    predictor = SAM2ModelPredictor(model=model)
     image_paths = predictor.pre_process_dataset(dataset)
-
-    # 🧠 Inférence
     results = predictor.run_inference_on_images(image_paths)
-
-    # 📤 Post-traitement en prédictions Picsellia
     predictions = predictor.post_process_results(image_paths, results, dataset)
 
-    # 📊 Évaluation Picsellia
     evaluate_model_impl(
         context=context,
         picsellia_predictions=predictions,
@@ -43,5 +60,3 @@ def evaluate(model: Model, dataset: CocoDataset):
         output_dir=os.path.join(context.working_dir, "evaluation"),
         training_labelmap=context.experiment.get_log("labelmap").data,
     )
-
-    print("✅ Évaluation SAM2 terminée et logguée.")
